9a1a306b69d06f14b46d7db38b5d39a219f697dd
Abogeerick <abogeerick@gmail.com>
2023-08-31 05:17:10 +0300

    Complete assignment

--- lib/models.py
+++ lib/models.py
@@ -1,21 +1,27 @@
-from sqlalchemy import func
-from sqlalchemy import Column, Integer, String, DateTime
+# models.py
+
+from datetime import datetime
+
+from sqlalchemy import create_engine, desc
+from sqlalchemy import Column, DateTime, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///migrations_test.db')
 
 Base = declarative_base()
 
-class Game(Base):
-    __tablename__ = 'games'
+class Student(Base):
+    __tablename__ = 'students'
 
     id = Column(Integer(), primary_key=True)
-    title = Column(String())
-    genre = Column(String())
-    platform = Column(String())
-    price = Column(Integer())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title="{self.title}", ' + \
-            f'platform="{self.platform})"'
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
